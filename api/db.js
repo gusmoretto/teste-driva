8106683c9da68af6f4dc13779a73dee2c35c8346
Gustavo Moretto <gustavomorettodias31@gmail.com>
2026-01-11 23:39:47 -0300

    Feat: setup inicial do banco de dados e conexao postgres

---
+++ api/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+require('dotenv').config();//Utiliza variáveis de ambiente do arquivo .env
+
+// Configuração da conexão com o banco de dados PostgreSQL
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+});
+
+module.exports = pool;// Exporta o pool de conexões para ser usado em outros módulos
